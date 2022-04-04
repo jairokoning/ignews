acaf1b44f18a904cf8a917cdb59d7eb096299fc1
Jairo Koning <jairo.koning@gmail.com>
2022-04-04 07:38:40 -0300

    feat: home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,28 @@
 import Head from 'next/head';
+import Image from 'next/image';
+
+import styles from './home.module.scss';
+import avatarImg from '../../public/images/avatar.svg';
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>ig.news | Inicio</title>  
+        <title>Home | ig.news</title>  
       </Head>
-      <h1>IG News</h1>    
+      
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>  Hey, welcome</span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+
+        <Image src={avatarImg} alt="Girl coding" />
+      </main>
     </>    
   )
 }
