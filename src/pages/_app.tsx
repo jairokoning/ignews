6ad466e9092a6a84e733ddcfc4cd243a2a5a8ae3
Jairo Koning <jairo.koning@gmail.com>
2022-04-07 06:53:28 -0300

    chore: configure github next auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,16 @@
 import { AppProps } from 'next/app';
 import { Header } from '../components/Header';
+import { SessionProvider as NextAuthSessionProvider } from 'next-auth/react';
 
 import "../styles/global.scss";
 
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <NextAuthSessionProvider session={pageProps.session}>
       <Header />
       <Component {...pageProps} />
-    </>
+    </NextAuthSessionProvider>
   )
 }
 
