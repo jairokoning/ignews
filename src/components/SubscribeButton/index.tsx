1c1cb79122aaf4947e1fc0f7fc3b621a4ed932de
Jairo Koning <jairo.koning@gmail.com>
2022-04-11 07:28:16 -0300

    feat: stripe checkout session

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,4 @@
+import { signIn, useSession } from 'next-auth/react';
 import styles from './styles.module.scss';
 
 interface SubscribeButtonProps {
@@ -5,6 +6,15 @@ interface SubscribeButtonProps {
 }
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const {status} = useSession();
+
+  function handleSubscribe() {
+    if (status !== 'authenticated') {
+      signIn('github')
+      return;
+    }
+  }
+
   return (
     <button
       type="button"
